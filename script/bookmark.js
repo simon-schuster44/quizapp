3ee66b96228b6811f3c2484a49f599848d9909c5
Justus-Becker-Rabbenstein <43116175+Justus-Becker-Rabbenstein@users.noreply.github.com>
2022-10-23 11:32:56 +0200

    changes logic for bookmark js

--- script/bookmark.js
+++ script/bookmark.js
@@ -1,10 +1,13 @@
 /* bookmark switch picture */
-const buttonBookmark = document.querySelector('[data-js=""]');
+const buttonBookmark = document.querySelector('[data-js="bookmark"]');
 
 buttonBookmark.addEventListener('click', function() {
-    if(buttonBookmark.src != "urspruenglichesBild") {
-        buttonBookmark.src = "neuesBild";
-    } else {
-        buttonBookmark.src = "urspruenglichesBild";
-    }
+
+if(buttonBookmark.classList.contains("fa-regular") == true) {
+    buttonBookmark.classList.remove("fa-regular");
+    buttonBookmark.classList.add("fa-solid");
+} else {
+    buttonBookmark.classList.remove("fa-solid");
+    buttonBookmark.classList.add("fa-regular");
+}
 })
