3a5d5e8d2572ef0017b747bb97e57c6e97f44260
Simon Schuster <simschu@gmx.de>
2022-10-29 00:07:28 +0200

    working js, new footer layout

--- script/form.js
+++ script/form.js
@@ -2,6 +2,7 @@ const overlay = document.querySelector('[data-js="overlay"]');
 const form = document.querySelector('[data-js="form"]');
 const button = document.querySelector('[data-js="button"]');
 const inputQ = document.querySelector("#question");
+const questionCards = [];
 
 function showOverlay() {
   overlay.classList.add("animate");
@@ -10,14 +11,26 @@ function showOverlay() {
 
 function hideOverlay() {
   overlay.classList.remove("animate");
+  inputQ.focus();
 }
-form.addEventListener("submit", (event) => {
+form.addEventListener("submit", async (event) => {
   event.preventDefault();
   const data = Object.fromEntries(new FormData(event.target));
-  questions.push(data);
-  console.log(questions);
+  const newQuestion = Object.assign(
+    {
+      number: questionCards.length,
+    },
+    data,
+    { isBookmarked: false }
+  );
+  questionCards.push(newQuestion);
   showOverlay();
-  inputQ.focus();
-});
 
-const questions = [];
+  for (const quest in questionCards) {
+    console.log(questionCards[quest].question);
+    let card = document.createElement("div");
+    card.classList.add("card");
+    card.textContent = questionCards[quest].question;
+    document.body.appendChild(card);
+  }
+});
