85200fe4b3e17894facccc2ca51c090e01d4f2f0
Leon Stechmann <Leon.Stechmann95@gmail.com>
2022-10-30 13:54:39 +0100

    characters left functionality in characters.js.

---
+++ script/characters.js
@@ -0,0 +1,30 @@
+const questionElement = document.querySelector('[data-js="questionInput"]');
+const answerElement = document.querySelector('[data-js="answerInput"]');
+const questionAmountLeft = document.querySelector(
+  '[data-js="questionAmountLeft"]'
+);
+const answerAmountLeft = document.querySelector('[data-js="answerAmountLeft"]');
+const questionMaxLength = questionElement.getAttribute("maxlength");
+const answerMaxLength = answerElement.getAttribute("maxlength");
+
+const updateQuestionAmountLeft = (value) => {
+  questionAmountLeft.innerText = value;
+};
+const updateAnswerAmountLeft = (value) => {
+  answerAmountLeft.innerText = value;
+};
+
+updateQuestionAmountLeft(questionMaxLength);
+updateAnswerAmountLeft(answerMaxLength);
+
+questionElement.addEventListener("input", () => {
+  updateQuestionAmountLeft(questionMaxLength - questionElement.value.length);
+});
+answerElement.addEventListener("input", () => {
+  updateAnswerAmountLeft(answerMaxLength - answerElement.value.length);
+});
+// html für den Text unter dem Input Form
+
+// <small class="form__character-count"
+// >(<span data-js="amountLeft"></span> characters left)
+// </small>
